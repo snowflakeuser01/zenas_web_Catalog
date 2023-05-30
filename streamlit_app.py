c17c934d78c2c6217cec880ed2705b72f2c0485c
snowflakeuser01 <134178963+snowflakeuser01@users.noreply.github.com>
2023-05-30 19:23:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,7 @@ my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
-streamlit.write(df)
+#streamlit.write(df)
+color_list = df[0].values.tolist()
+print(color_list)
 
